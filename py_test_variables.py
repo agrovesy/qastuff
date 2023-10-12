648393f1db864d34bd9a6a27374bbcccbe08a463
adam groves <adam.groves@synergy-logistics.co.uk>
2023-10-12 15:21:26 +0100

    work from qa

---
+++ py_test_variables.py
@@ -0,0 +1,20 @@
+print("hello_world")
+
+#username='Bob'
+#age=32
+#print(f"{username} is, {age},'years old'")
+
+
+username=input('Please enter your name')
+age = input('Please enter your age')
+print(f"{username} is, {age},'years old'")
+
+length= float(input('Please the length of the triangle: '))
+width = float(input('Please the width of the triangle:'))
+
+Perimeter = 2 * (length + width)
+
+print(Perimeter)
+
+Area= length * width
+print(Area)
